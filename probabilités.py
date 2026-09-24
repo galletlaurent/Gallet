4660801b407917a5d66569983989c43d1bae2959
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-24 22:00:15 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -2236,7 +2236,6 @@ with tab1:
             file_name=f"Rapport_Atelier1_{nom_eleve_local_at1}.html",
             mime="text/html",
             key="btn_exporter1_download_local_at1_final_stable_v8",
-            disabled=identite_invalide_at1,
         )
     # =====================================================================
     # LIGNE 1486 : LE TITRE DE L'EXERCICE (Revenez bien aligné tout à gauche)
