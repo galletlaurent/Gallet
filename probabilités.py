399263f7a54c8b796c2911ffa396285b7bb1b739
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-25 16:19:45 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -167,13 +167,6 @@ with tab0:
                 st.rerun()
 
 with tab1:
-    # 1. GARDE-FOU SÉCURITÉ : Bloque l'accès si l'élève n'a pas validé l'accueil
-    if not st.session_state.get("verrouille", False):
-        st.warning(
-            "Acces restreint : Veuillez d'abord valider votre identite dans l'onglet 'Identification'."
-        )
-        st.stop()
-
     st.header("1. Jeux de hasard 1 : Dé et Jeu de 32 cartes")
 
     # =========================================================================
