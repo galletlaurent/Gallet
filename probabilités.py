ab54b0f8e4f0b36b76b5cf7f57b5c570efb594a2
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-25 21:13:40 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -451,7 +451,8 @@ with tab2:
         st.session_state.slot_verdict = None
     if "slot_stats_gains" not in st.session_state:
         st.session_state.slot_stats_gains = {"JACKPOT": 0, "PETIT GAIN": 0, "PERDU": 0}
-
+    if "roulette_dernier_numero" not in st.session_state:
+    st.session_state.roulette_dernier_numero = 0
     # Séparation en deux colonnes maîtresses étanches
     col_master_roulette, col_master_slot = st.columns(2)
 
