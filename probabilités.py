c27707c87d3ec516c8ca6d966135b02fcee483a8
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-26 19:07:00 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -1122,7 +1122,7 @@ with tab1:
 
         st.download_button(
             label="CLIQUEZ ICI POUR ENREGISTRER LE RAPPORT SUR VOTRE ORDINATEUR",
-            data=data=html_content,,
+            data=data=html_content,
             file_name=f"{nom_fichier_clean}.html",
             mime="text/html",
             use_container_width=True
