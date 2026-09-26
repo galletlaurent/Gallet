26e613d5797862070c27291350fb1a3c6ca65d1d
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-26 22:21:41 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -2926,8 +2926,6 @@ with tab5:
             st.text_input("tot_ex_in", value=st.session_state.get("cell_at5_8", ""), key="cell_at5_8", label_visibility="collapsed", disabled=st.session_state.at5_verrouille)
             if sol_at5: style_cellule_at5("cell_at5_8", sol_at5["E_X"])
 
-            
-        st.write("<div style='margin-top:15px;'></div>", unsafe_allow_html=True)
         cv_1, cv_2 = st.columns(2)
         with cv_1:
             st.write("**Esperance Mathematique E(X) :**")
@@ -2941,20 +2939,15 @@ with tab5:
             if sol_at5:
                 style_cellule_at5("cell_at5_vx", sol_at5["V_X"], tolerance=0.05)
 
-        # 2. ZONE UNIQUE DES BOUTONS DE CORRECTION (PLANCÉE APRÈS LA VARIANCE)
-        st.write("---")
-        c_b1, c_b2 = st.columns(2)
-        with c_b1:
-            if st.button("Corriger l'exercice", key="btn_at5_corr_visuelle", disabled=st.session_state.at5_verrouille, use_container_width=True):
+                
+        st.write("<div style='margin-top:10px;'></div>", unsafe_allow_html=True)
+
+
+        if st.button("VERIFIER LES REPONSES DU TABLEAU", key="btn_verifier_grille_at5", disabled=st.session_state.at5_verrouille, use_container_width=True):
+            if "at5_scenario" not in st.session_state:
+                st.error("Veuillez d'abord generer un exercice avec le bouton en haut.")
+            else:
                 st.session_state.at5_afficher_correction = True
-                st.rerun()
-        with c_b2:
-            if st.button("Effacer tout", key="btn_at5_raz", disabled=st.session_state.at5_verrouille, use_container_width=True):
-                st.session_state.at5_afficher_correction = False
-                for idx_clr in range(1, 10):
-                    st.session_state[f"cell_at5_{idx_clr}"] = ""
-                st.session_state["cell_at5_ex"] = ""
-                st.session_state["cell_at5_vx"] = ""
                 st.rerun()
     # =========================================================================
     # RE-INJECTION DES QUESTIONNAIRES MELANGES ET INVERSES SANS COPIE
