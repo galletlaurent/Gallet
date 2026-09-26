b9a54dbc6880aedb9818c655668e4946b3d1c3c6
galletlaurent <laurent.gallet@ac-lille.fr>
2026-09-26 22:06:08 +0200

    Add files via upload

--- probabilités.py
+++ probabilités.py
@@ -2926,20 +2926,21 @@ with tab5:
             st.text_input("tot_ex_in", value=st.session_state.get("cell_at5_8", ""), key="cell_at5_8", label_visibility="collapsed", disabled=st.session_state.at5_verrouille)
             if sol_at5: style_cellule_at5("cell_at5_8", sol_at5["E_X"])
 
-    # Blocs Espérance et Variance finaux
+        st.write("<div style='margin-top:15px;'></div>", unsafe_allow_html=True)
+        cv_1, cv_2 = st.columns(2)
         with cv_1:
             st.write("**Esperance Mathematique E(X) :**")
             st.text_input("ex_final", value=st.session_state.get("cell_at5_ex", ""), key="cell_at5_ex", label_visibility="collapsed", disabled=st.session_state.at5_verrouille)
-            if sol_at5: 
+            if sol_at5:
                 style_cellule_at5("cell_at5_ex", sol_at5["E_X"])
                 
         with cv_2:
             st.write("**Variance Geometrique V(X) :**")
             st.text_input("vx_final", value=st.session_state.get("cell_at5_vx", ""), key="cell_at5_vx", label_visibility="collapsed", disabled=st.session_state.at5_verrouille)
-            if sol_at5: 
+            if sol_at5:
                 style_cellule_at5("cell_at5_vx", sol_at5["V_X"], tolerance=0.05)
-
-    # CORRECTIF D'ALIGNEMENT : On sort de with cv_2 et with col_d_table_at5 pour un affichage propre
+                
+    # # CORRECTIF D'ALIGNEMENT : On sort de with cv_2 et with col_d_table_at5 pour un affichage propre
     st.write("---")
     c_b1, c_b2 = st.columns(2)
     with c_b1:
@@ -2949,7 +2950,7 @@ with tab5:
     with c_b2:
         if st.button("Effacer tout", key="btn_at5_raz", disabled=st.session_state.at5_verrouille, use_container_width=True):
             st.session_state.at5_afficher_correction = False
-            for idx_clr in range(1, 10): 
+            for idx_clr in range(1, 10):
                 st.session_state[f"cell_at5_{idx_clr}"] = ""
             st.session_state["cell_at5_ex"] = ""
             st.session_state["cell_at5_vx"] = ""
